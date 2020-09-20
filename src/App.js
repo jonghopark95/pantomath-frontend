f5af50a18b79d14291357ed525ef0529291d32c4
jonghopark95 <jong951005@gmail.com>
2020-09-20 15:23:49 +0900

    네비게이션 바 수정

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const MainContainer = styled.div`
   width: 100vw;
   height: 100vh;
   overflow-y: hidden;
+  .swiper-pagination-bullets {
+    top: 42% !important;
+  }
+  .swiper-pagination-bullet-active {
+    background-color: rgb(47, 54, 64);
+  }
 `;
 
 const App = () => (
